74b7e5dd0c27210f54389e16602d28f6d47dba3d
MichaelDobosh <Muxa_Dobosh@mail.ru>
2023-02-04 19:37:32 +0800

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const greetings = () => {
+  console.log('Welcome to the Brain Games!');
   const yourName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${yourName}!`);
   return yourName;
