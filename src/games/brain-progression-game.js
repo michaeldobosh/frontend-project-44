3088dcf9a8bfb4b70e00c3b40b0f581b2b3ca99f
MichaelDobosh <Muxa_Dobosh@mail.ru>
2023-02-04 22:46:33 +0800

    refactoring

--- src/games/brain-progression-game.js
+++ src/games/brain-progression-game.js
@@ -1,34 +1,35 @@
 import gameRun from '../index.js';
 
+const random = () => Math.ceil(Math.random() * 10);
+const random2 = () => Math.ceil(Math.random() * 5);
+const progression = (interval) => {
+  const numbers = [random()];
+  for (let i = 0; i <= 10; i += 1) {
+    numbers.push(numbers[i] + interval);
+    if (numbers.length >= 5) {
+      if (Math.ceil(Math.random() * 4) === 2) break;
+    }
+  }
+  return numbers;
+};
+let outputNum = progression(random2());
+let index = 0;
+let hideNumber = 0;
+const question = () => {
+  index = Math.floor(Math.random() * outputNum.length);
+  hideNumber = outputNum[index];
+  outputNum[index] = '..';
+  return `Question: ${outputNum.join(' ')}`;
+};
+const correct = () => {
+  outputNum = progression(random2());
+  index = Math.floor(Math.random() * outputNum.length);
+  return String(hideNumber);
+};
 function brainProgressionGame() {
   const description = 'What number is missing in the progression.';
-  const random = () => Math.ceil(Math.random() * 10);
-  const random2 = () => Math.ceil(Math.random() * 5);
-  const progression = (interval) => {
-    const numbers = [random()];
-    for (let i = 0; i <= 10; i += 1) {
-      numbers.push(numbers[i] + interval);
-      if (numbers.length >= 5) {
-        if (Math.ceil(Math.random() * 4) === 2) break;
-      }
-    }
-    return numbers;
-  };
-  let outputNum = progression(random2());
-  let index = 0;
-  let hideNumber = 0;
-  const question = () => {
-    index = Math.floor(Math.random() * outputNum.length);
-    hideNumber = outputNum[index];
-    outputNum[index] = '..';
-    return `Question: ${outputNum.join(' ')}`;
-  };
-  const correct = () => {
-    outputNum = progression(random2());
-    index = Math.floor(Math.random() * outputNum.length);
-    return String(hideNumber);
-  };
-
+  question();
+  correct();
   gameRun(description, correct, question);
 }
 
