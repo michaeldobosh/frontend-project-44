12ea220a026803d83093cd2748c1cd2c05cfa45d
MichaelDobosh <Muxa_Dobosh@mail.ru>
2023-02-04 19:21:57 +0800

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,30 +1,34 @@
 import readlineSync from 'readline-sync';
 
-function gameRun(description, correct, question) {
+const greetings = () => {
   const yourName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${yourName}!`);
-  const gameAnswer = () => {
-    console.log(description);
-    for (let i = 0; i < 3; i += 1) {
-      const rand = [];
-      for (let j = 0; j < 2; j += 1) {
-        rand.push(Math.ceil(Math.random() * 50));
-      }
-      console.log(question(...rand));
-      const answer = readlineSync.question('Your answer: ');
-      const correctAnswer = correct(...rand);
-      if (answer === correctAnswer) {
-        console.log('Correct!');
-        if (i === 2) console.log(`Congratulations, ${yourName}!`);
-      } else {
-        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
-        console.log(`Let's try again, ${yourName}!`);
-        break;
-      }
+  return yourName;
+};
+const userName = greetings();
+const comparison = (correct, question, name) => {
+  for (let i = 0; i < 3; i += 1) {
+    const rand = [];
+    for (let j = 0; j < 2; j += 1) {
+      rand.push(Math.ceil(Math.random() * 50));
     }
-  };
+    console.log(question(...rand));
+    const answer = readlineSync.question('Your answer: ');
+    const correctAnswer = correct(...rand);
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+      if (i === 2) console.log(`Congratulations, ${name}!`);
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
+      console.log(`Let's try again, ${name}!`);
+      break;
+    }
+  }
+};
 
-  gameAnswer();
+function gameRun(description, correct, question) {
+  console.log(description);
+  comparison(correct, question, userName);
 }
 
 export default gameRun;
