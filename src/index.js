72b9ef7eaa1224cc876b40c1a73f49d418d3eca0
MichaelDobosh <Muxa_Dobosh@mail.ru>
2023-02-15 12:57:52 +0800

    refactoring index.js

--- src/index.js
+++ src/index.js
@@ -5,16 +5,15 @@ const gameRuls = (description, lounch) => {
   const userName = greetings();
   console.log(description);
   for (let i = 0; i < 3; i += 1) {
-    const up = Math.ceil;
-    const rand = [up(Math.random() * 50), up(Math.random() * 50)];
+    const rand = [Math.ceil(Math.random() * 50), Math.ceil(Math.random() * 50)];
     const [question, answer] = lounch(...rand);
     console.log(`Question: ${question}`);
     const response = readlineSync.question('Your answer: ');
-    const correctAnswer = answer;
-    if (response === correctAnswer) {
+    if (response === answer) {
       console.log('Correct!');
     } else {
-      return console.log(`'${response}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${userName}!`);
+      return console.log(`'${response}' is wrong answer ;(. Correct answer was '${answer}'. 
+      \nLet's try again, ${userName}!`);
     }
   }
   return console.log(`Congratulations, ${userName}!`);
